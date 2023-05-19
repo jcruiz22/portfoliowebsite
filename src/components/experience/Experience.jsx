254dc6ece232ecaed28c28f041132d6efa64ff34
jcruiz22 <jcruizm22@gmail.com>
2023-05-19 13:49:03 -0600

    prueba

---
+++ src/components/experience/Experience.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './experience.css'
+
+function Exeperience() {
+  return (
+    <section id='experience'>Exeperience</section>
+  )
+}
+
+export default Exeperience
